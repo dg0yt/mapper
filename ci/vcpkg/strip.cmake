fe4258c80b8f51f32201f0a405375735ba72f3ed
Kai Pastor <dg0yt@darc.de>
2026-03-21 10:18:23 +0100

    WIP

--- ci/vcpkg/strip.cmake
+++ ci/vcpkg/strip.cmake
@@ -1,21 +1,22 @@
 find_program(STRIP_COMMAND NAMES strip REQUIRED)
 
 file(GLOB_RECURSE binaries LIST_DIRECTORIES false
-    "${CURRENT_PACKAGES_DIR}/lib"
-    "${CURRENT_PACKAGES_DIR}/plugins/lib"
-    "${CURRENT_PACKAGES_DIR}/tools"
-    "${CURRENT_PACKAGES_DIR}/debug/lib"
-    "${CURRENT_PACKAGES_DIR}/debug/plugins"
-    "${CURRENT_PACKAGES_DIR}/tools"
+    "${CURRENT_PACKAGES_DIR}/lib/*"
+    "${CURRENT_PACKAGES_DIR}/plugins/lib/*"
+    "${CURRENT_PACKAGES_DIR}/tools/*"
+    "${CURRENT_PACKAGES_DIR}/debug/lib/*"
+    "${CURRENT_PACKAGES_DIR}/debug/plugins/*"
+    "${CURRENT_PACKAGES_DIR}/tools/*"
 )
 file(GLOB_RECURSE data LIST_DIRECTORIES false
-    "${CURRENT_PACKAGES_DIR}/lib/pkgconfig"
-    "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig"
+    "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/*"
+    "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/*"
 )
 list(REMOVE_ITEM binaries ${data} "")
 foreach(file IN LISTS binaries)
     string(FIND "${file}" "/tools/" tools_index)
-    if(tools_index EQUAL -1 OR IS_EXECUTABLE "${file}")
+    #if(tools_index EQUAL -1 OR IS_EXECUTABLE "${file}")
+        message(STATUS "Running '${STRIP_COMMAND} -S ${file}'")
         execute_process(COMMAND "${STRIP_COMMAND}" -S "${file}")
-    endif()
+    #endif()
 endforeach()
