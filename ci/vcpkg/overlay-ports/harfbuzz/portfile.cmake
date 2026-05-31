d5c8afbf887a687c967d21f1b56340fa6718bd86
Kai Pastor <dg0yt@darc.de>
2026-05-31 17:54:35 +0200

    harfbuzz(android)

--- ci/vcpkg/overlay-ports/harfbuzz/portfile.cmake
+++ ci/vcpkg/overlay-ports/harfbuzz/portfile.cmake
@@ -76,6 +76,10 @@ else()
     list(APPEND OPTIONS -Dintrospection=disabled)
 endif()
 
+if(VCPKG_TARGET_IS_ANDROID)
+    list(APPEND OPTIONS -Dwith_libstdcxx=true)
+endif()
+
 set(cxx_link_libraries "")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     block(PROPAGATE cxx_link_libraries)
